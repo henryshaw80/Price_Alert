6f903b989adfc75d83b336967ce3a0176f216a84
Henry Shaw <henryshaw80@gmail.com>
2016-12-21 17:13:31 +0800

    create users/user.py
    users/user.py

---
+++ src/models/users/user.py
@@ -0,0 +1,14 @@
+__author__ = 'Timur'
+
+import uuid
+import _datetime
+from src.common.database import Database
+
+class User(object):
+    def __init__(self, email, password, _id=None):
+        self.email = email
+        self.password = password
+        self._id = uuid.uuid4().hex if _id is None else _id
+
+    def __repr__(self):
+        return "<User {}>".format(self.email)
