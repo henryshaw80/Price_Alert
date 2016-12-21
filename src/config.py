52a2f09cbe95ef582b482af181c19ad17196f8a3
Henry Shaw <henryshaw80@gmail.com>
2016-12-21 17:10:59 +0800

    create config.py
    configuration for debug and admin e-mail address

---
+++ src/config.py
@@ -0,0 +1,8 @@
+__author__ = 'Timur'
+
+DEBUG = True
+# frozen set is a constant list with no duplicate values
+# if something goes wrong, the admin will be informed
+ADMINS = frozenset([
+    "hipstercamper@gmail.com"
+])
