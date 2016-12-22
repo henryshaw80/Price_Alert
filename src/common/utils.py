7add46b6039f9b5d5a4a2ebf6b7dbb655b49ffe7
Henry Shaw <henryshaw80@gmail.com>
2016-12-22 16:20:14 +0800

    add hash password method
    hash_password

--- src/common/utils.py
+++ src/common/utils.py
@@ -5,6 +5,19 @@ from passlib.hash import pbkdf2_sha512
 class Utils(object):
 
     @staticmethod
+    def hash_password(password):
+        """
+        Hashes a password using pbkdf2_sha512
+        :param password: sha512 password from the login/register form
+        :return: a sha512->pbkdf2_sha512 encrypted password
+        """
+        return pbkdf2_sha512.encrypt(password)
+
+        # difference between pbkdf2_sha512 vs sha512
+        # pbkdf2_sha512: can be decrypted, but encryption always change
+        # sha512: cannot be decrypted, but encryption always fixed
+
+    @staticmethod
     def check_hashed_password(password, hashed_password):
         """
         Checks that password the user sent matches that of the database
