5a2c1cac8a2acb108caae89512d2e630b26a87e2
Henry Shaw <henryshaw80@gmail.com>
2016-12-22 17:23:05 +0800

    remove duplication
    (1) create a UserError class with an init that specifies the
    self.message= message
    (2) use UserError in all other classes

--- src/models/users/errors.py
+++ src/models/users/errors.py
@@ -1,6 +1,6 @@
 __author__ = 'Timur'
 
-class UserNotExistError(Exception):
+class UserError(Exception):
     # This call will contain exceptions in server-side
     # server-side can raise an exception, which will be pass on
     # to client-side, which will see the exception message
@@ -8,6 +8,10 @@ class UserNotExistError(Exception):
     def __init__(self, message):
         self.message = message
 
-class IncorrectPasswordError(Exception):
-    def __init__(self, message):
-        self.message = message
+class UserNotExistError(UserError):
+    # The init has been specified in UserError class that captures all exception
+    pass
+
+class IncorrectPasswordError(UserError):
+    # The init has been specified in UserError class that captures all exception
+    pass
