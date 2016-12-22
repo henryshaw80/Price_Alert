b8f20ba5addad8ce417fe9e560d5fc0311cf5793
Henry Shaw <henryshaw80@gmail.com>
2016-12-22 17:26:53 +0800

    add exception handler
    (1) surround is_login_valid method with "try-except" (exception handler)
    (2) remove duplication on the the exception handler

--- src/models/users/views.py
+++ src/models/users/views.py
@@ -5,6 +5,7 @@ __author__ = 'Timur'
 
 from flask import Blueprint, request, url_for, render_template, session, redirect
 from src.models.users.user import User
+import src.models.users.errors as UserErrors
 
 # __name__ is unique to this file when the app is running
 user_blueprint = Blueprint('users', __name__)
@@ -18,18 +19,29 @@ def login_user():
         email = request.form['email']
         password = request.form['hashed']
 
-        if User.is_login_valid(email, password):
-            # put email into session (temporary storage of data)
-            # when a browser arrives at our webpage, we give them
-            # a unique identifier. the browser will use this identifier
-            # with every request to let the webpage know that the browser
-            # is related to this email.
-            session['email'] = email
+        try:
+            if User.is_login_valid(email, password):
+                # put email into session (temporary storage of data)
+                # when a browser arrives at our webpage, we give them
+                # a unique identifier. the browser will use this identifier
+                # with every request to let the webpage know that the browser
+                # is related to this email.
+                session['email'] = email
 
-            return redirect(url_for(".user_alerts"))
-            # url_for is getting the url for a specific method
-            # redirect is '301', a HTTP status code that tells the browser
-            # to change to other location (i.e. address).
+                return redirect(url_for(".user_alerts"))
+                # url_for is getting the url for a specific method
+                # redirect is '301', a HTTP status code that tells the browser
+                # to change to other location (i.e. address).
+
+        # old code: catch a unique exception then return message
+        # except UserErrors.UserNotExistError as e: Catch user does not exist error
+        #    return e.message
+        # except UserErrors.IncorrectPasswordError as e: Catch incorrect pswd error
+        #    return e.message
+        # new code: remove duplication
+        # we only use UserError class that captures all exceptions
+        except UserErrors.UserError as e:
+            return e.message
 
     # if request method is not 'POST', go to login page
     # if request method is 'POST AND login is invalid, go to login page again
