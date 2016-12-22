22bbfc299c7436c00c7ba37d5e1c7a0f849aa1fe
Henry Shaw <henryshaw80@gmail.com>
2016-12-22 16:19:47 +0800

    add user_blueprint to API
    from src.models.users.views import user_blueprint
    app.register_blueprint(user_blueprint, url_prefix="/users")

--- src/app.py
+++ src/app.py
@@ -2,12 +2,27 @@ __author__ = 'Timur'
 # app.py stores the app but it runs from run.py
 
 from flask import Flask
+from src.common.database import Database
 
 app = Flask(__name__)
+
 # import config to be loaded to app
 app.config.from_object('config')
 
-@app.route('/')
-def hello_world():
-    return "Hello, world!"
+# allow session to be secured; when a browser comes into an application
+# and request of a webpage; Flask will put a cookie with a session id
+# to link that browser to a specific session in our server.
+# and that session in our server that contains session's email
+# in order to secure those cookies, we need a secret key
+app.secret_key = "123" #usually 32 random characters and numbers
+
+
+# Initialize database
+@app.before_first_request
+def init_db():
+    Database.initialize()
+
+# import user_blueprint
+from src.models.users.views import user_blueprint
+app.register_blueprint(user_blueprint, url_prefix="/users")
 
