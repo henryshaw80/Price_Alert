38d3db1ef941ee2a9f3af887c37d2bf0cd5c382d
Henry Shaw <henryshaw80@gmail.com>
2016-12-22 12:31:11 +0800

    add password check
    check_hashed_password(password, hashed_password)

--- src/common/utils.py
+++ src/common/utils.py
@@ -1,2 +1,16 @@
 __author__ = 'Timur'
 
+from passlib.hash import pbkdf2_sha512
+
+class Utils(object):
+
+    @staticmethod
+    def check_hashed_password(password, hashed_password):
+        """
+        Checks that password the user sent matches that of the database
+        Database password is encrypted more thatn the user's password at this stage
+        :param password:  sha512 hashed password
+        :param hashed_password: pbkdf2 encrypted password
+        :return: True is password match, False otherwise
+        """
+        return pbkdf2_sha512.verify(password, hashed_password)
