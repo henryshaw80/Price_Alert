855d14de92f76900fe36f844cf12bc6a7fd40d53
Henry Shaw <henryshaw80@gmail.com>
2016-12-22 12:30:48 +0800

    add login check method
    is_login_valid(email, password)

--- src/models/users/user.py
+++ src/models/users/user.py
@@ -12,3 +12,22 @@ class User(object):
 
     def __repr__(self):
         return "<User {}>".format(self.email)
+
+    @staticmethod
+    def is_login_valid(email, password):
+        """
+        This method verifies that an e-mail/password combo (as sent by the site forms) is valid.
+        Checks that the e-mail exists, and that the password associated to that e-mail is correct.
+        :param email: The user's email
+        :param password: A sha512 hashed password
+        :return: True if valid, False otherwise
+        """
+        user_data = Database.find_one("users",{"email":email})
+        if user_data is None:
+            # tell user that their e-mail doesn't exist
+            pass
+        if not Utils.check_hashed_password(password, user_data['password']):
+            # tell user that their password is wrong
+            pass
+
+        return True
