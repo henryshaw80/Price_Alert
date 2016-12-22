6ca72fca4cb0f6c39be6b4a18bb36cec77578a7a
Henry Shaw <henryshaw80@gmail.com>
2016-12-22 12:07:30 +0800

    Revert "add requests and passlib"
    This reverts commit 7efa5bb915cd682dc67f033c2e54ac27b756c1f4.

--- src/common/utils.py
+++ src/common/utils.py
@@ -1,16 +1,2 @@
 __author__ = 'Timur'
 
-from passlib.hash import pbkdf2_sha512
-
-class Utils(object):
-
-    @staticmethod
-    def check_hashed_password(password, hashed_password):
-        """
-        Checks that the password the user sent matches that of the database
-        The database password is encrypted more than the user's password at this stage
-        :param password: sha512 hashed password
-        :param hashed_password:  pbkdf2_sha512 encrypted password
-        :return: True if passwords match, False otherwise
-        """
-        return pbkdf2_sha512.verify(password, hashed_password)
--- src/models/users/user.py
+++ src/models/users/user.py
@@ -12,29 +12,3 @@ class User(object):
 
     def __repr__(self):
         return "<User {}>".format(self.email)
-
-    @staticmethod
-    def is_login_valid(email, password):
-        """
-        This method verifies that an e-mail/password combo
-        (as sent by the site form) is valid.
-        Checks that e-mail exists, and that the password
-        associated to that e-mail is correct.
-        :param email: the user's email
-        :param password: a sha512 hashed password
-        :return: True if valid, False otherwise
-        """
-        # pull user's information from database
-        user_data = Database.find_one("users", {"email":email})
-
-        if user_data is None:
-            # tell user that their e-mail doesn't exist
-            pass
-
-        if not Utils.check_hashed_password(password, user_data['password']):
-            # tell user that their password is wrong
-            pass
-
-        # when email and password are valid
-        return True
-
