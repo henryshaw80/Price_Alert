422de5445955182d96b8f178d4d382c2c0ccc492
Henry Shaw <henryshaw80@gmail.com>
2016-12-22 12:51:38 +0800

    Create errors.py
    for storing exceptions

---
+++ src/models/users/errors.py
@@ -0,0 +1,11 @@
+__author__ = 'Timur'
+
+class UserNotExistError(Exception):
+
+    # This call will contain exceptions in server-side
+    # server-side can raise an exception, which will be pass on
+    # to client-side, which will see the exception message
+    # (e.g. user does not exist message)
+    def __init__(self, message):
+        self.message = message
+
