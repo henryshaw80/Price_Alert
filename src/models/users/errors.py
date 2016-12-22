18d7b03f37d05ad41c22b724d5408427da3f5339
Henry Shaw <henryshaw80@gmail.com>
2016-12-22 16:21:11 +0800

    add errors message methods
    IncorrectPasswordError(Exception)

--- src/models/users/errors.py
+++ src/models/users/errors.py
@@ -1,7 +1,6 @@
 __author__ = 'Timur'
 
 class UserNotExistError(Exception):
-
     # This call will contain exceptions in server-side
     # server-side can raise an exception, which will be pass on
     # to client-side, which will see the exception message
@@ -9,3 +8,6 @@ class UserNotExistError(Exception):
     def __init__(self, message):
         self.message = message
 
+class IncorrectPasswordError(Exception):
+    def __init__(self, message):
+        self.message = message
