a9c45710e740064741a5ccbfcd014de69a882a35
Henry Shaw <henryshaw80@gmail.com>
2016-12-23 17:31:51 +0800

    add email identifier
    using regular expression to identify email pattern

--- src/common/utils.py
+++ src/common/utils.py
@@ -1,8 +1,14 @@
 __author__ = 'Timur'
 
 from passlib.hash import pbkdf2_sha512
+import re #import regular expression
 
 class Utils(object):
+
+    @staticmethod
+    def email_is_valid(email):
+        email_address_matcher = re.compile('^[\w-]+@([\w-]+\.)+[\w]+$')
+        return True if email_address_matcher.match(email) else False
 
     @staticmethod
     def hash_password(password):
