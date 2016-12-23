6762e564bc7465069c49f22aea08b2b1403b473e
Henry Shaw <henryshaw80@gmail.com>
2016-12-23 17:32:48 +0800

    remove web scrapping
    move web scrapping method to item.py

--- src/models/items/views.py
+++ src/models/items/views.py
@@ -9,13 +9,6 @@ item_blueprint = Blueprint('items', __name__)
 def item_page():
     pass
 
-@item_blueprint.route('/load')
-def load_item():
-    """
-    Loads an item's data using their store and return a JSON representation
-    :return:
-    """
-    pass
 
 
 
