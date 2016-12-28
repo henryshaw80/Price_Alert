ae21eef1296122db90e1d6768e88d66c9e0a0f7c
Henry Shaw <henryshaw80@gmail.com>
2016-12-28 17:46:47 +0800

    modify alert.py

--- src/models/alerts/alert.py
+++ src/models/alerts/alert.py
@@ -1,10 +1,13 @@
 __author__ = 'Timur'
 
+import uuid
+
 class Alert(object):
-    def __init__(self, user, price_limit, item):
+    def __init__(self, user, price_limit, item, _id=None):
         self.user = user
         self.price_limit = price_limit
         self.item = item
+        self._id = uuid.uuid4().hex if _id is None else _id
 
     # define what they look like, how they are being printed
     # out using print function; their string representation
