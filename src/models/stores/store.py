f7383c96c97384d264991efb570d1f12ac1c5ff5
Henry Shaw <henryshaw80@gmail.com>
2016-12-28 17:48:14 +0800

    retrieve stores by id, name and url_prefix

--- src/models/stores/store.py
+++ src/models/stores/store.py
@@ -1,9 +1,69 @@
 __author__ = 'Timur'
 
+import uuid
+from src.common.database import Database
+import src.models.stores.constants as StoreConstants
+import src.models.stores.errors as StoreErrors
+
 class Store(object):
-    def __init__(self, name, url_prefix):
+    def __init__(self, name, url_prefix, tag_name, query, _id=None):
         self.name = name
         self.url_prefix = url_prefix
+        self.tag_name = tag_name
+        self.query = query
+        self._id = uuid.uuid4().hex if _id is None else _id
 
     def __repr__(self):
         return "<Store {}>".format(self.name)
+
+    def json(self):
+        return {
+            "_id": self._id,
+            "name": self.name,
+            "url_prefix": self.url_prefix,
+            "tag_name": self.tag_name,
+            "query": self.query
+        }
+
+    @classmethod
+    def get_by_id(cls, id):
+        return cls(**Database.find_one(collection=StoreConstants.COLLECTION,
+                                       query={"_id":id}))
+
+    def save_to_mongo(self):
+        Database.insert(collection=StoreConstants.COLLECTION,
+                        data=self.json())
+
+    @classmethod
+    def get_by_name(cls, store_name):
+        return cls(**Database.find_one(collection=StoreConstants.COLLECTION,
+                                      query={'name': store_name}))
+
+    @classmethod
+    def get_by_url_prefix(cls, url_prefix):
+        """
+        matching url_prefix using mongodb's regex operator
+        for example: http://www.john -> http://www.johnlewis.com -> Store("John Lewis")
+        :param url_prefix: URL prefix
+        :return: a Store class that matches with URL prefix
+        """
+        return cls(**Database.find_one(collection=StoreConstants.COLLECTION,
+                                   query={'url_prefix': {"$regex": '^{}'.format(url_prefix)}}))
+
+    @classmethod
+    def find_by_url(cls, url):
+        """
+        Return a store from a url like "http://www.johnLewis.com/kin-by-john-lewis-wool-blend-peacoat-navy/p3033177"
+        :param url: the item's URL
+        :return: a Store, or raises a StoreNotFoundException if no store matches the URL
+        """
+        for i in range(0, len(url)+1):
+            try:
+                store = cls.get_by_url_prefix(url[:i])
+                return store
+            except:
+                # all python method will return None if we don't specify
+                # to return anything, so we can simply type pass.
+                # alternatively, we can raise an exception
+                raise StoreErrors.StoreNotFoundException("The URL Prefix used to find the store didn't give us any results!")
+
