37aead092f7f5bfab56e50c3d2bf9bbb543683af
Henry Shaw <henryshaw80@gmail.com>
2016-12-29 15:51:52 +0800

    create alerts/constants.py
    setting up URL, API-key and SMTP Login using mailgun Sandbox server's
    parameters.

---
+++ src/models/alerts/constants.py
@@ -0,0 +1,10 @@
+__author__ = 'Timur'
+
+# API Base URL
+URL = "https://api.mailgun.net/v3/sandbox892cb079cb034eb29dfe7a044f3bf39a.mailgun.org/messages"
+
+# API Key
+API_KEY = "key-b21fe4a81c8a4e144267a63e45e05f0f"
+
+# Default SMTP Login
+FROM = "Mailgun Sandbox postmaster@sandbox892cb079cb034eb29dfe7a044f3bf39a.mailgun.org"
