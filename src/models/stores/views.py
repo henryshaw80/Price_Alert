f81d5c94b58dbb99e61fb4693f9fa1daad2965c3
Henry Shaw <henryshaw80@gmail.com>
2017-01-04 16:09:17 +0800

    add index to stores_blueprint

--- src/models/stores/views.py
+++ src/models/stores/views.py
@@ -5,6 +5,10 @@ from flask import Blueprint
 # __name__ is unique to this file when the app is running
 store_blueprint = Blueprint('stores', __name__)
 
+@store_blueprint.route('/')
+def index():
+    return "This is the stores index"
+
 @store_blueprint.route('/store/<string:name>')
 def store_page():
     pass
