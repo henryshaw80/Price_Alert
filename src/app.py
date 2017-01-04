f47369b21324b0e50279cc6a83df77b6382c7fce
Henry Shaw <henryshaw80@gmail.com>
2017-01-04 17:21:32 +0800

    import and register blueprints

--- src/app.py
+++ src/app.py
@@ -26,7 +26,12 @@ def init_db():
 def home():
     return render_template('home.jinja2')
 
-# import user_blueprint
+# import blueprints
 from src.models.users.views import user_blueprint
+from src.models.alerts.views import alert_blueprint
+from src.models.stores.views import store_blueprint
+# register the blueprints
 app.register_blueprint(user_blueprint, url_prefix="/users")
+app.register_blueprint(alert_blueprint, url_prefix="/alerts")
+app.register_blueprint(store_blueprint, url_prefix="/stores")
 
