e82380fe8c1f92e297c38d7a0272e05f2acb91f2
Henry Shaw <henryshaw80@gmail.com>
2017-01-10 15:23:22 +0800

    add requires_login decorator

--- src/models/alerts/views.py
+++ src/models/alerts/views.py
@@ -3,6 +3,7 @@ __author__ = 'Timur'
 from flask import Blueprint, render_template, request, session
 from src.models.alerts.alert import Alert
 from src.models.items.item import Item
+import src.models.users.decorators as user_decorators
 
 alert_blueprint = Blueprint('alerts', __name__)
 
@@ -11,6 +12,8 @@ def index():
     return "This is the alerts index"
 
 @alert_blueprint.route('/new/', methods =['GET', 'POST'])
+@user_decorators.requires_login
+# @requires_login decorator to redirect the user to 'users.login' if session['email'] is None
 def create_alert():
     if request.method == 'POST':
         name = request.form['name']
@@ -28,16 +31,14 @@ def create_alert():
     return render_template('alerts/new_alert.jinja2')
 
 @alert_blueprint.route('/deactivate/<string:alert_id>')
+@user_decorators.requires_login
 def deactivate_alert(alert_id):
     pass
 
 @alert_blueprint.route('/<string:alert_id>') #/alerts/<alert_id>
+@user_decorators.requires_login
 def get_alert_page(alert_id):
     alert = Alert.find_by_id(alert_id)
     return render_template('alerts/alert.jinja2', alert=alert)
-
-@alert_blueprint.route('/for_user/<string:user_id>')
-def get_alerts_for_user(user_id):
-    pass
 
 
