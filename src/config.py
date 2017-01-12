58b2ad9ff0bea583ebd8e48112da6f7ec324ed93
Henry Shaw <henryshaw80@gmail.com>
2017-01-12 16:39:49 +0800

    change DEBUG to False

--- src/config.py
+++ src/config.py
@@ -1,9 +1,10 @@
 __author__ = 'Timur'
 
-DEBUG = True
+DEBUG = False
 # frozen set is a constant list with no duplicate values
 # if something goes wrong, the admin will be informed
 # in a frozenset, the elements cannot be added or removed
 ADMINS = frozenset([
     "hipstercamper@gmail.com"
 ])
+
