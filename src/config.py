ec214d61cc2cabfd0cd8adc98d82dd23958b8321
Henry Shaw <henryshaw80@gmail.com>
2017-01-12 13:46:35 +0800

    add comment

--- src/config.py
+++ src/config.py
@@ -3,6 +3,7 @@ __author__ = 'Timur'
 DEBUG = True
 # frozen set is a constant list with no duplicate values
 # if something goes wrong, the admin will be informed
+# in a frozenset, the elements cannot be added or removed
 ADMINS = frozenset([
     "hipstercamper@gmail.com"
 ])
